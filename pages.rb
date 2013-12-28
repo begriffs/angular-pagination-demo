c1ffc27d0129e54b3d7478e5a142356358cc2937
Joe Nelson <cred+github@begriffs.com>
2013-12-28 00:32:24 -0800

    Integers endpoint

--- pages.rb
+++ pages.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'andand'
 
 get '/' do
   "Hello World!"
+end
+
+get '/integers' do
+  range = request.env['HTTP_CONTENT_RANGE'].andand.match(/(?<min>\d+)[ ]*-[ ]*(?<max>\d+)/)
+  if range
+    min, max = range['min'].to_i, range['max'].to_i
+    if request.env['HTTP_RANGE_UNIT'] == 'items'
+      body '[' + (min .. max).to_a.join(', ') + ']'
+    else
+      status 416
+      body 'Unknown or unspecified Range-Unit (use "items")'
+    end
+  else
+    status 413 # request entity too large
+  end
 end
