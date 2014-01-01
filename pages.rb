f021f33cf623aa3f8494d372f80b16640e7546cd
Joe Nelson <cred+github@begriffs.com>
2013-12-31 16:21:07 -0800

    Pull pagination into its own function

--- pages.rb
+++ pages.rb
@@ -1,22 +1,49 @@
 require 'sinatra'
 require 'andand'
+require 'slim'
 
 get '/' do
-  "Hello World!"
+  slim :index
 end
 
-get '/integers' do
-  range = request.env['HTTP_CONTENT_RANGE'].andand.match(/(?<min>\d+)[ ]*-[ ]*(?<max>\d+)/)
+def paginate limit
+  headers 'Accept-Ranges' => 'items'
+
+  range = request.env['HTTP_RANGE'].andand.match(/(?<min>\d+)[ ]*-[ ]*(?<max>\d+)/)
   if range
     min, max = range['min'].to_i, range['max'].to_i
     if request.env['HTTP_RANGE_UNIT'] == 'items'
-      headers 'Link' => "</integers>; rel=\"next\"; items=\"#{1 + max}-#{1 + max*2 - min}\""
-      body '[' + (min .. max).to_a.join(', ') + ']'
+      if 0 <= max - min && max - min < (limit || Float::INFINITY)
+        headers 'Link' => "</integers>; rel=\"next\"; items=\"#{1 + max}-#{1 + max*2 - min}\""
+        headers 'Content-Range' => "items #{min}-#{max}/#{limit || '*'}"
+        status 206
+        yield min, max
+      else
+        status 416 # invalid range
+      end
     else
       status 416
       body 'Unknown or unspecified Range-Unit (use "items")'
     end
   else
-    status 413 # request entity too large
+    yield
+  end
+end
+
+get '/integers' do
+  paginate 100 do |min, max|
+    if min && max
+      body '[' + (min .. max).to_a.join(', ') + ']'
+    else
+      status 413 # result too large
+    end
+  end
+end
+
+get '/alphabet' do
+  paginate 26 do |min, max|
+    min ||= 0
+    max ||= 25
+    body '[' + ( (min .. max).map { |i| "'" + (i+'a'.ord).chr + "'" } ).join(', ') + ']'
   end
 end
