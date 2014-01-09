5ff5d9280724004bdc7b7f2fcc1bf7870c1a2238
Joe Nelson <cred+github@begriffs.com>
2014-01-08 21:21:23 -0800

    Include angular-resource

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,13 @@ requirejs.config({
     'angular': { exports: 'angular' }
   },
   paths: {
-    angular: '/components/angular/angular.min'
+    'angular': '/components/angular/angular.min',
+    'angular-resource': '/components/angular-resource/angular-resource.min'
   }
 });
 
 require(
-  [
-    'angular'
-  ],
+  [ 'angular', 'angular-resource' ],
   function (angular) {
     angular.module('pages', []);
     angular.bootstrap(document, ['pages']);
