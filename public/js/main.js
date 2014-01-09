bd27d1aea3bd38dd0bcd67ef597b19f86e4facd9
Joe Nelson <cred+github@begriffs.com>
2014-01-08 23:28:29 -0800

    Load first page of letters and integers

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,42 @@
 requirejs.config({
   shim: {
-    'angular': { exports: 'angular' }
+    'angular': { exports: 'angular' },
+    "angular-resource": {
+      deps: ["angular"]
+    }
   },
   paths: {
-    'angular': '/components/angular/angular.min',
-    'angular-resource': '/components/angular-resource/angular-resource.min'
+    'angular': '/components/angular/angular',
+    'angular-resource': '/components/angular-resource/angular-resource'
   }
 });
 
 require(
   [ 'angular', 'angular-resource' ],
-  function (angular) {
-    angular.module('pages', []);
-    angular.bootstrap(document, ['pages']);
+  function (ng) {
+    ng.module('pagination', ['ngResource']);
+    ng.module('pagination').
+      factory('paginated-resource', ['$resource', function(resource) {
+        return function (url, params, actions) {
+          actions = ng.extend(actions || {}, {
+            query: {
+              isArray: true,
+              method: 'GET',
+              headers: {
+                'Range-Unit': 'items',
+                'Range': '0-9'
+              }
+            }
+          });
+
+          return resource(url, params, actions);
+        };
+      }]).
+      controller('FunCtrl', ['$scope', 'paginated-resource', function(scope, resource) {
+        scope.letters = resource('/alphabet').query();
+        scope.integers = resource('/integers').query();
+      }]);
+
+    ng.bootstrap(document, ['pagination']);
   }
 );
